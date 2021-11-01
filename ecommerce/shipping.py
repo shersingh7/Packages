bd59fff196d3c05fd2199bc5c388ef7e028f15d7
Davinder <66485246+shersingh7@users.noreply.github.com>
2021-11-01 15:25:41 -0400

    Initial commit

---
+++ ecommerce/shipping.py
@@ -0,0 +1,2 @@
+def cal_shipping():
+    print("Cal_Shipping")
---
+++ main.py
@@ -0,0 +1,7 @@
+# 1st method
+import ecommerce.shipping
+ecommerce.shipping.cal_shipping()
+
+# 2nd method
+from ecommerce.shipping import cal_shipping
+cal_shipping()
